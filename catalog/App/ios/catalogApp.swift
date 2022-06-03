8b48a511f541c10c5a5a93889cef58d770c00e32
Furuta Katsumi <withcomputer@gmail.com>
2022-06-03 20:52:51 +0900

    [add] Xcodeプロジェクトの追加

---
+++ catalog/App/ios/catalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  catalogApp.swift
+//  Shared
+//
+//  Created by Katsumi Furuta on 2022/06/03.
+//
+
+import SwiftUI
+
+@main
+struct catalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

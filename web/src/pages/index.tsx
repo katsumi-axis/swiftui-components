7240686eeb5f66e4e2e71edc8be685cf766a4e68
Furuta Katsumi <withcomputer@gmail.com>
2022-06-05 22:07:11 +0900

    [remove] verification

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,12 +1,10 @@
 import type { NextPage } from 'next'
 import HomePage from '@/components/pages'
 import CommonMeta from '@/utils/CommonMeta'
-import Head from 'next/head'
 
 const Home: NextPage = () => {
     return (
         <>
-            <Head><meta name="google-site-verification" content="J26VwaLt7zFMMB0EuDrg_in-dxunfjdODoJHL036IVk" /></Head>
             <CommonMeta />
             <HomePage />
         </>
